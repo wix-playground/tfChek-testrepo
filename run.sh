7bf57e17c0f1e8d6cd62a28f1e8d8ad49e19ebea
Maksym Shkolnyi <maksymsh@wix.com>
2019-10-15 13:22:14 +0300

    Add run.sh

---
+++ run.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+: "${TIMEOUT:=300}"
+
+echo -e "\033[0;35mThis is a mock run.sh for tfChek\033[0m"
+
+echo -e "\033[0;33mThe command will continue to run for $TIMEOUT seconds\033[0m"
+
+echo -e "\033[0;32mCommand has been run with following arguments\033[0m"
+
+echo -e "\033[0;31m$0 $@\033[0m"
+
+let i=0
+while [[ $i -lt $TIMEOUT ]]
+do 
+  echo -n -e "\033[0;3$(($i%10))m."
+  sleep 1
+  let i=i+1
+done
+
+echo -e "\033[0;32mEnd of the program\033[0m"
+ 
+

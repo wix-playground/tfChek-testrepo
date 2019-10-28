45a66c553480f047db4cb909caeaa5d8efc48da2
Maksym Shkolnyi <maksymsh@wix.com>
2019-10-28 09:46:49 +0200

    Fix run.sh

--- run.sh
+++ run.sh
@@ -1,11 +1,6 @@
 #!/bin/bash
 
 : "${TIMEOUT:=30}"
-<<<<<<< HEAD
-
-=======
-wekfjhweklfjwelkf
->>>>>>> master
 echo -e "\033[0;35mThis is a mock run.sh for tfChek\033[0m"
 
 echo -e "\033[0;33mThe command will continue to run for $TIMEOUT seconds\033[0m"
